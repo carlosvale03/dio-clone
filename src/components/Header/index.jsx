88d74046a1cc9521c873f3089c4a523070e611b8
carlosvale03 <dovalecarlos03@gmail.com>
2023-12-29 11:24:29 -0300

    Criação do header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import log from "../../assets/logo-dio.png"
+
+import {
+    BuscarInputContainer,
+    Container,
+    Input,
+    Menu,
+    MenuRigth,
+    Row,
+    Wrapper
+} from "./styles"
+import { Button } from "../Button"
+
+const Header = () => {
+  return (
+    <Wrapper>
+        <Container>
+            <Row>
+                <img src={log} alt='Logo da dio' />
+                <BuscarInputContainer>
+                    <Input placeholder='Buscar...' />
+                </BuscarInputContainer>
+                <Menu>Live code</Menu>
+                <Menu>Global</Menu>
+            </Row>
+            <Row>
+                <MenuRigth href='#'>Home</MenuRigth>
+                <Button title="Entrar" />
+                <Button title="Cadastrar" />
+            </Row>
+        </Container>
+    </Wrapper>
+  )
+}
+
+export { Header }

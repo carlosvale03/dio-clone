02ab5298c5638fb8fc3c9d7477de5388b35bcf45
carlosvale03 <dovalecarlos03@gmail.com>
2023-12-29 16:04:00 -0300

    Criação da tela de home

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -10,12 +10,19 @@ export const ButtonContainer = styled.button`
     min-width: 120px;
     width: 100%;
 
+    &:hover {
+        opacity: 0.6;
+        cursor: pointer;
+    }
 
-    ${({variant}) => variant !== "primary" && css`
+
+    ${({ variant }) => variant !== "primary" && css`
         min-width: 167px;
         height: 33px;
 
         background: #E4105D;
+
+        
 
         &::after {
             content: '';
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,40 @@
-
 import { Header } from "../../components/Header"
+
+// banner original
+// import bannerImage from "../../assets/banner.png"
+// banner ney
+import bannerImage from "../../assets/ney.png"
+
+import {
+    Container, 
+    TextContent, 
+    Title, 
+    TitleHighlight 
+} from "./styles"
+import { Button } from "../../components/Button"
+
 
 const Home = () => {
     return (<>
         <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare 
+                    seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant="secundary" onClick={() => null} />
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem principal" />
+            </div>
+        </Container>
     </>)
 }
 
